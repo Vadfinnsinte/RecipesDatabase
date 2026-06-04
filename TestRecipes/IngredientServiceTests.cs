5fdd11436395c2c1e37ad3a49709aea64e4ce0ee
Linda Westerlund <lindawesterlund94@gmail.com>
2026-06-04 15:49:11 +0200

    Added tests and a genericResult for better return values and easier testing

---
+++ TestRecipes/IngredientServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using NSubstitute;
+using recipes.Interfaces;
+using recipes.Models;
+using recipes.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestRecipes
+{
+    public class IngredientServiceTests
+    {
+        // Checks if mock repo accepts, and returns expected data
+        // Checks if values was what was expected
+        [Fact]
+        public async Task GetByRecipeId_Should_ReturnIngredients()
+        {
+            var repo = Substitute.For<IIngredientRepository>();
+            var service = new IngredientService(repo);
+
+            repo.GetByRecipeIdAsync(1).Returns(new List<Ingredient>
+            {
+                new Ingredient { Name = "Flour", RecipeId = 1 }
+            });
+
+            var result = await service.GetByRecipeIdAsync(1);
+
+            await repo.Received(1).GetByRecipeIdAsync(1);
+            var item = Assert.Single(result);
+            Assert.Equal("Flour", item.Name);
+            Assert.Equal(1, item.RecipeId);
+
+        }
+
+    }
+}
